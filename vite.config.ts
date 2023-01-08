8ddd20db14c202f37b17b0976accabd0ed05994a
Shubham Pawar <shubhampawar3007@gmail.com>
2023-01-08 10:25:19 +0530

    refactor(lint): Replace tslint with eslint
    Since tslint has been deprecated in favor of eslint.
    I've also included few more plugins(react-jsx-ally,etc) to make it more strict.
    Also added nabla eslint to vite so we can get error reporting in terminal itself.
    There are few more rules that I would like to discuss before moving forward
    
    Tools used:
    - https://github.com/typescript-eslint/tslint-to-eslint-config
    - https://medium.com/palantir/tslint-in-2019-1a144c2317a9

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from '@nabla/vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslintPlugin()],
+  server: { port: 3000 },
 })
