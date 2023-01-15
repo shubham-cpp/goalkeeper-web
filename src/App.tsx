11c32c65b8241da90427d71c67d9d52314de5710
Chinmay Ghungurde <54586237+azrael3199@users.noreply.github.com>
2023-01-15 19:22:29 +0530

    feat: login and signup pages (#17)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import routes from './routes'
+import theme from './theme'
 
 const router = createBrowserRouter(routes)
 
+const queryClient = new QueryClient()
+
 function App(): JSX.Element {
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  )
 }
 
 export default App
--- src/Sample.tsx
+++ src/Sample.tsx
@@ -1,32 +1,39 @@
+import { Box } from '@chakra-ui/react'
 import { useState } from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
 
 function Sample(): JSX.Element {
   const [count, setCount] = useState(0)
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <div className="App">
-          <div>
-            <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-              <img src="/vite.svg" className="logo" alt="Vite logo" />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={(): void => setCount((count: number): number => count + 1)}>count is {count}</button>
-            <p>
-              Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </div>
-      </ChakraProvider>
-    </QueryClientProvider>
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={(): void => setCount((count: number): number => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <Box padding={30}>
+          <Box bg="malachite" height={50} padding={10}>
+            Malachite
+          </Box>
+          <Box bg="midnightGreen" height={50} padding={10}>
+            Midnight Green
+          </Box>
+          <Box bg="isabelline" height={50} padding={10}>
+            Isabelline
+          </Box>
+          <Box bg="ripeMango" height={50} padding={10}>
+            Ripe Mango
+          </Box>
+        </Box>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </div>
   )
 }
 
