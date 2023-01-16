8796d417654863f154859f76505181ee2dee9da6
azrael3199 <chinmayghungurde@gmail.com>
2023-01-16 10:49:23 +0530

    fix: extend the theme object

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -11,7 +11,7 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider theme={extendTheme(theme)}>
         <App />
       </ChakraProvider>
     </QueryClientProvider>
